b5c51731b87be0d73540a344d6e159763bd04d6d
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2025-03-31 18:25:03 +0200

    wip: add serviece page

--- config/node/frontend/src/App.js
+++ config/node/frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './home/Home';
+import Services from './services/Services';
 
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Services />
 
     </div>
   );
